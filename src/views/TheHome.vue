07b2eb62b69ffb365efe25977f5f7e2a7b1e7aa9
pengz <2972564513@qq.com>
2022-06-27 18:22:36 +0800

    feat: 基础框架搭建

---
+++ src/views/TheHome.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="the-home">
+    <div class="the-home-menu"></div>
+    <div class="the-home-content"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHome'
+}
+</script>
+
+<style scoped lang="scss">
+  @import "../assets/style/common";
+.the-home {
+  height: 100%;
+  width: 100%;
+
+  .the-home-menu {
+    height: $--top-menu-height;
+    width: 100%;
+    box-shadow: 5px 5px 15px $--shadow-color-primary;
+  }
+
+  .the-home-content {
+    width: 100%;
+    height: calc(100% - 48px);
+  }
+}
+</style>
